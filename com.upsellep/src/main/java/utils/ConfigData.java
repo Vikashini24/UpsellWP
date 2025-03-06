b881f9426a06116637478488a38af22746ef5027
Vikashini24 <kanniyavikashini@gmail.com>
2025-03-06 13:28:07 +0530

    Initial commit

---
+++ com.upsellep/src/main/java/utils/ConfigData.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigData {
+	
+	FileInputStream fis;
+	Properties property;
+	
+	public ConfigData() {
+		try {
+			fis = new FileInputStream("/home/cartrabbit/eclipse-workspace/UpsellWP/com.upsellep/src/main/java/resources/data.properties");
+			property = new Properties();
+			property.load(fis);
+		}catch(IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Failed to load data.properties file");
+		}
+	}
+	
+	public String getProperty(String key) {
+		String datas = property.getProperty(key);
+		return datas;
+		
+	}
+	
+}
