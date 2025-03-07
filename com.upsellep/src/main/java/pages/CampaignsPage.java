8c9b1e78ee56d67bb79573452c1a95ab9bd1c756
Vikashini24 <kanniyavikashini@gmail.com>
2025-03-07 17:24:08 +0530

    campaigns

--- com.upsellep/src/main/java/pages/CampaignsPage.java
+++ com.upsellep/src/main/java/pages/CampaignsPage.java
@@ -1,21 +1,35 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import utils.ConfigData;
+
 public class CampaignsPage extends BasePage {
+	
+	ConfigData config = new ConfigData();
 	
 	@FindBy(css=".create-campaign")
 	WebElement createCampaignButton;
 	
+	//div[@class='card-body py-1 px-4 text-center']/following-sibling::button/parent::a/div/h4[text()='Checkout Upsells']
+	//div[@class='card-body py-1 px-4 text-center']/following-sibling::button/parent::a/div/h4[text()='" + campaignname + "']
+	String campainsName = config.getProperty("campaignname");
 	
+	//tr[@data-title='Email Upsell']/td/div
 	
 	
 	public CampaignsPage(WebDriver driver) {
 		super(driver);
 	}
 	
+	public void createCampaigns() {
+		createCampaignButton.click();
+		WebElement campaings = driver.findElement(By.xpath("//div[@class='card-body py-1 px-4 text-center']/following-sibling::button/parent::a/div/h4[text()='\" + campaignname + \"']"));
+		campaings.click();
+	}
 	
 	
 }
---
+++ com.upsellep/src/main/java/pages/CheckoutUpsell.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutUpsell extends BasePage {
+	
+	
+	
+	public CheckoutUpsell(WebDriver driver) {
+		super(driver);
+	}
+	
+	
+	
+}
