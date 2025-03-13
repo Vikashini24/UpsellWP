3a3f647249ad408d0ed2b7f175e34ff71fdea63e
Vikashini24 <kanniyavikashini@gmail.com>
2025-03-13 18:32:51 +0530

    checkout

--- com.upsellwp/src/main/java/pages/CheckoutUpsell.java
+++ com.upsellwp/src/main/java/pages/CheckoutUpsell.java
@@ -1,11 +1,19 @@
 package pages;
 
+import java.util.List;
+
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import utils.ConfigData;
 
 public class CheckoutUpsell extends BasePage {
 	
+	CampaignsPage campaignspage = new CampaignsPage(driver);
+	ConfigData config = new ConfigData();
 	
 	@FindBy(xpath="//span[text()='Checkout Upsells']")
 	WebElement campaignNameText;
@@ -20,7 +28,7 @@ public class CheckoutUpsell extends BasePage {
 	WebElement displayMethodDropdown;
 	
 	@FindBy(id="cuw-offers")
-	WebElement offersList;
+	List<WebElement> offersList;
 	
 	@FindBy(id="offer-add")
 	WebElement addOfferButton;
@@ -48,8 +56,28 @@ public class CheckoutUpsell extends BasePage {
 		super(driver);
 	}
 	
+	public void enterCampaignName() {
+		if((campaignspage.campaignsName).equals(campaignNameText)) {
+			campaignTitleField.sendKeys(config.getProperty("campaigns"));
+		}
+	}
 	
+	public void selectDisplayLocation() {
+		Select dropdown = new Select(displayLocationDropdown);
+		dropdown.selectByVisibleText("Before Payment Gateways");
+	}
 	
+	public void selectDisplayMethod() {	
+		Select dropdown = new Select(displayMethodDropdown);
+		dropdown.selectByValue("all");
+	}
 	
+	public void listOfOffers() {
+		int offerCount = offersList.size();
+		if(offerCount<10) {
+			addOfferButton.click();
+		}
+		
+	}
 	
 }
