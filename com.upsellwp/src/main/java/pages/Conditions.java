d9e981ad4f613ba09308e1b1e3d43e4f83cde03c
Vikashini24 <kanniyavikashini@gmail.com>
2025-03-17 17:50:09 +0530

    conditions cart a

--- com.upsellwp/src/main/java/pages/Conditions.java
+++ com.upsellwp/src/main/java/pages/Conditions.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
+import utils.ConfigData;
+
 public class Conditions extends BasePage{
+	
+	ConfigData config = new ConfigData();
 	
 	Select conditionsdropdown;
 	Select conditionsListDropdown;
 	Select lineItemNumberListDropdown;
+	Select orderStatusDropdown;
 	
 	@FindBy(id="add-condition")
 	WebElement addConditionButton;
@@ -23,12 +28,19 @@ public class Conditions extends BasePage{
 	@FindBy(xpath="//div[@class='condition-operator flex-fill']/select")
 	WebElement chooseLineItemNumberList;
 	
+	@FindBy(xpath="//div[@class='condition-value']/input")
+	WebElement totalAmountField;
+	
+	@FindBy(xpath="//div[@class='condition-values']/select")
+	WebElement orderStatusField;
+	
 	
 	public Conditions(WebDriver driver) {
 		super(driver);
 		conditionsdropdown = new Select(chooseConditionDropdown);
 		conditionsListDropdown = new Select(chooseConditionsListDrodown);
 		lineItemNumberListDropdown = new Select(chooseLineItemNumberList);
+		//orderStatusDropdown = new 
 	}
 	
 	public void addCondition() {
@@ -142,7 +154,175 @@ public class Conditions extends BasePage{
 		lineItemNumberListDropdown.selectByVisibleText("Range (from-to)");
 	}
 	
-	//
+	//Coupons list
+	//InList NotinList - above
+	public void anyCoupon() {
+		conditionsListDropdown.selectByVisibleText("Any coupon");
+	}
+	
+	public void noCoupons() {
+		conditionsListDropdown.selectByVisibleText("No coupons");
+	}
+	
+	//FirstOrder
+	public void firstorderYes() {
+		conditionsListDropdown.selectByVisibleText("Yes");
+	}
+	
+	public void firstorderNo() {
+		conditionsListDropdown.selectByVisibleText("No");
+	}
+	
+	//Number of orders made
+	 public void currentDay() {
+		 lineItemNumberListDropdown.selectByVisibleText("Current day");
+	 }
+	 
+	 public void currentWeek() {
+		 lineItemNumberListDropdown.selectByVisibleText("Current week");
+	 }
+	 
+	 public void currentMonth() {
+		 lineItemNumberListDropdown.selectByVisibleText("Current month");
+	 }
+	 
+	 public void currentYear() {
+		 lineItemNumberListDropdown.selectByVisibleText("Current year");
+	 }
+	 
+	 public void oneDay() {
+		 lineItemNumberListDropdown.selectByVisibleText("1 day");
+	 }
+	 
+	 public void twoDays() {
+		 lineItemNumberListDropdown.selectByVisibleText("2 days");
+	 }
+	 
+	 public void threeDays() {
+		 lineItemNumberListDropdown.selectByVisibleText("3 days");
+	 }
+	 
+	 public void fourDays() {
+		 lineItemNumberListDropdown.selectByVisibleText("4 days");
+	 }
+	 
+	 public void fiveDays() {
+		 lineItemNumberListDropdown.selectByVisibleText("5 days");
+	 }
+	 
+	 public void sixDays() {
+		 lineItemNumberListDropdown.selectByVisibleText("6 days");
+	 }
+	 
+	 public void oneWeek() {
+		 lineItemNumberListDropdown.selectByVisibleText("1 week");
+	 }
+	 
+	 public void twoWeeks() {
+		 lineItemNumberListDropdown.selectByVisibleText("2 weeks");
+	 }
+	 
+	 public void threeWeeks() {
+		 lineItemNumberListDropdown.selectByVisibleText("3 weeks");
+	 }
+	 
+	 public void fourWeeks() {
+		 lineItemNumberListDropdown.selectByVisibleText("4 weeks");
+	 }
+	 
+	 public void oneMonth() {
+		 lineItemNumberListDropdown.selectByVisibleText("1 month");
+	 }
+	 
+	 public void twoMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("2 months");
+	 }
+	 
+	 public void threeMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("3 months");
+	 }
+	 
+	 public void fourMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("4 months");
+	 }
+	 
+	 public void fiveMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("5 months");
+	 }
+	 
+	 public void sixMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("6 months");
+	 }
+	 
+	 public void sevenMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("7 months");
+	 }
+	 
+	 public void eightMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("8 months");
+	 }
+	 
+	 public void nineMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("9 months");
+	 }
+	 
+	 public void tenMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("10 months");
+	 }
+	 
+	 public void elevenMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("11 months");
+	 }
+	 
+	 public void twelveMonths() {
+		 lineItemNumberListDropdown.selectByVisibleText("12 months");
+	 }
+	 
+	 public void twoYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("2 years");
+	 }
+	 
+	 public void threeYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("3 years");
+	 }
+	 
+	 public void fourYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("4 years");
+	 }
+	 
+	 public void fiveYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("5 years");
+	 }
+	 
+	 public void sixYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("6 years");
+	 }
+	 
+	 public void sevenYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("7 years");
+	 }
+	 
+	 public void eightYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("8 years");
+	 }
+	 
+	 public void nineYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("9 years");
+	 }
+	 
+	 public void tenYears() {
+		 lineItemNumberListDropdown.selectByVisibleText("10 years");
+	 }
+	 
+	public void enterTotalAmount() {
+		totalAmountField.sendKeys(config.getProperty("totalamount"));
+	}
+	
+	//OrderStatus
+	
+	
+	 
+	 
 	
 	
 }
